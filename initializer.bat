bb6abb2fe45e38af6f15e8ed86e336331d160887
Bae <hortock@outlook.com>
2025-06-11 18:50:30 -0300

    Update initializer.bat

--- initializer.bat
+++ initializer.bat
@@ -1,3 +1,3 @@
 @echo off
-pwsh -c "& $env:HOME/shapeCounter/setup.ps1"
-pwsh -c "& $env:HOME/shapeCounter/launcher.ps1"
+pwsh -c "& $env:HOME\shapeCounter\setup.ps1"
+pwsh -c "& $env:HOME\shapeCounter\launcher.ps1"
